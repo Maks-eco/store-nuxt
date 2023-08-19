4d63d33a402b08fb511c121cdcada6efa07b6bf8
Maks-eco <maks.zawulon@gmail.com>
2023-08-19 13:38:23 +0400

    Updates

--- assets/basket.2ca87afe.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./entry.ad48c2fc.js";const n={};function o(r,s){return t(),c("div",null,"Basket")}const _=e(n,[["render",o]]);export{_ as default};
---
+++ assets/basket.6a4424e7.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./entry.8c109c1f.js";const n={};function o(r,s){return t(),c("div",null,"Basket")}const _=e(n,[["render",o]]);export{_ as default};
