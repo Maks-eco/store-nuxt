aac8599abeaaa39613137aaf08f738a73a4205cf
Maks-eco <maks.zawulon@gmail.com>
2023-09-14 18:34:15 +0400

    add card v-hover, clear unused styles

--- components/product/Card.vue
+++ components/product/Card.vue
@@ -4,12 +4,12 @@ const store = useCounterStore();
 
 const expand = ref(false);
 
-const props = defineProps<{
+const globalProps = defineProps<{
   item: ConfProducts;
   brandList: Category[] | null;
 }>();
-const cardInfo = ref(props.item as ConfProducts);
-const productVariantSelected = ref(props.item.type === "simple");
+const cardInfo = ref(globalProps.item as ConfProducts);
+const productVariantSelected = ref(globalProps.item.type === "simple");
 
 const envUrl = (url: string) => {
   // if (process.env.NODE_ENV !== "development") return baseUrl.slice(0, -1) + url;
@@ -20,8 +20,10 @@ const envUrl = (url: string) => {
 };
 
 const productBrand = (id: number) => {
-  if (props.brandList) {
-    const brand = props.brandList.find((item: Category) => item.id === id);
+  if (globalProps.brandList) {
+    const brand = globalProps.brandList.find(
+      (item: Category) => item.id === id
+    );
     return brand?.title;
   }
   return "";
@@ -40,142 +42,61 @@ const updateImageEvent = (product: ProductFeature) => {
     product.image.slice(0, 6) + "s" + product.image.slice(6);
   cardInfo.value.productFeature = { ...product };
 };
-
-onMounted(() => {
-  // store.getCategories().then((data: any) => {
-  //   brandList.value = [...data] as Category[];
-  // });
-});
 </script>
 
 <template>
-  <v-card width="230" class="m-5" elevation="3">
-    <v-img
-      height="230"
-      src="images/autumn-pumpkin-watercolor62.png"
-      cover
-      class="text-white"
-      :style="{}"
+  <v-hover v-slot="{ isHovering, props }" close-delay="150">
+    <v-card
+      width="230"
+      class="mx-5 trans-it"
+      :class="isHovering ? ' mt-5 mb-7' : ' mt-7 mb-5'"
+      elevation="3"
+      v-bind="props"
     >
-      <v-img :src="envUrl(cardInfo.image)" :style="{}"> </v-img>
-    </v-img>
+      <v-img height="230" src="images/autumn-pumpkin-watercolor62.png" cover>
+        <v-img :src="envUrl(cardInfo.image)" :style="{}"> </v-img>
+      </v-img>
 
-    <v-card-text class="pb-1">
-      <div class="font-weight-bold ms-1 mb-2">{{ cardInfo.title }}</div>
-      <div class="ms-1">{{ productBrand(cardInfo.brand) }}</div>
-      <div class="ms-1">${{ cardInfo.regular_price.value.toFixed(2) }}</div>
-    </v-card-text>
-    <div v-if="props.item.type !== 'simple'">
-      <v-expand-transition>
-        <div v-if="expand">
-          <ProductAcceptButton
-            @on-click-act="userAcceptProduct"
-            :productVariantSelected="productVariantSelected"
-          />
+      <v-card-text class="pb-1">
+        <div class="font-weight-bold ms-1 mb-2">{{ cardInfo.title }}</div>
+        <div class="ms-1">{{ productBrand(cardInfo.brand) }}</div>
+        <div class="ms-1">${{ cardInfo.regular_price.value.toFixed(2) }}</div>
+      </v-card-text>
+      <div v-if="globalProps.item.type !== 'simple'">
+        <v-expand-transition>
+          <div v-if="expand">
+            <ProductAcceptButton
+              @on-click-act="userAcceptProduct"
+              :productVariantSelected="productVariantSelected"
+            />
 
-          <VariantsIcon
-            :item="cardInfo"
-            @update-image="updateImageEvent"
-            @variant-selected="productVariantState"
-          />
-        </div>
-      </v-expand-transition>
+            <VariantsIcon
+              :item="cardInfo"
+              @update-image="updateImageEvent"
+              @variant-selected="productVariantState"
+            />
+          </div>
+        </v-expand-transition>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-card-actions class="py-1">
-        <v-btn @click="expand = !expand">
-          {{ !expand ? "Выберите опции" : "Свернуть" }}
-        </v-btn>
-      </v-card-actions>
-    </div>
-    <ProductAcceptButton
-      @on-click-act="userAcceptProduct"
-      :productVariantSelected="true"
-      v-else
-    />
-  </v-card>
+        <v-card-actions class="py-1">
+          <v-btn @click="expand = !expand">
+            {{ !expand ? "Выберите опции" : "Свернуть" }}
+          </v-btn>
+        </v-card-actions>
+      </div>
+      <ProductAcceptButton
+        @on-click-act="userAcceptProduct"
+        :productVariantSelected="true"
+        v-else
+      />
+    </v-card>
+  </v-hover>
 </template>
 
 <style lang="scss" scoped>
-.aftr-opac:after {
-  content: "";
-  opacity: ".5";
-}
-.container-item {
-  display: flex;
-  width: 200px;
-  margin: 20px 20px 20px 20px;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
-.description {
-  margin-left: 20px;
-  width: 160px;
-}
-.image {
-  aspect-ratio: 1 / 1;
-  width: 100%;
-  object-fit: cover;
-  transition: 0.3s all ease-in-out;
-}
-.container-item:hover .image {
-  transform: scale(1.05);
-}
-.title {
-  font-weight: 700;
-}
-.brand-title {
-  height: 15px;
-  font-size: 0.8rem;
-  margin-bottom: 5px;
-}
-.btn-con {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-.button {
-  height: 35px;
-  width: 100%;
-  padding-top: 7px;
-  padding-bottom: 7px;
-  background-color: #fff;
-  border-radius: 0 0 10px 10px;
-  font-size: 0.8rem;
-  cursor: pointer;
-
-  @media (min-width: 480px) {
-    color: $secondary-inactive;
-    border: 1px solid $secondary-inactive;
-    border-top: 0;
-  }
-  @media (max-width: 480px) {
-    color: $active-color;
-    border: 1px solid $active-color;
-    border-top: 0;
-  }
-}
-
-$outl-color: $active-color;
-$ow: 0.1px;
-
-.container-item:hover .button {
-  color: $active-color;
-  border: 1px solid $active-color;
-  border-top: 0;
-
-  /*   text-shadow: $ow $ow 0 $outl-color, $ow -$ow 0 $outl-color,
-    -$ow $ow 0 $outl-color, -$ow -$ow 0 $outl-color, $ow 0px 0 $outl-color,
-    0px $ow 0 $outl-color, -$ow 0px 0 $outl-color, 0px -$ow 0 $outl-color; */
-
-  transition: all 0.3s;
-}
-
-.container-item:hover .button-inactive {
-  border: 1px solid $secondary-inactive;
-  border-top: 0;
-  color: $active-color;
-  cursor: default;
+.trans-it {
+  transition: all 0.3s ease;
 }
 </style>
