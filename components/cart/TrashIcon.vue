b820a2ddf5decb6022d91ea8caf588c6752f51bb
Maks-eco <maks.zawulon@gmail.com>
2023-09-15 23:28:20 +0400

    mobile category menu on vuetify, upd icons

---
+++ components/cart/TrashIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const emit = defineEmits(["clickBtn"]);
+</script>
+
+<template>
+  <!-- <button class="cart-button" @click="emit('clickBtn')">
+    <img class="trash-img" src="/images/trash.svg" alt="product preview" />
+  </button> -->
+  <v-btn icon="mdi-trash-can-outline" @click="emit('clickBtn')" />
+</template>
+
+<style lang="scss" scoped>
+.cart-button {
+  background-color: #fff;
+  border: none;
+  color: white;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  transition: opacity 0.2s;
+  opacity: 0.4;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+.trash-img {
+  height: 30px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import { useDisplay } from "vuetify";
-// import { mdiDotsVertical } from "@mdi/js";
-// import "@mdi/font/css/materialdesignicons.css";
 const store = useCounterStore();
 const route = useRoute();
 const hideCart = ref(true);
 const hideCategory = ref(true);
+const menuButton = ref(false);
 
 const { smAndUp } = useDisplay();
+
+const toggle = () => {
+  console.log("menuButton = !menuButton", menuButton.value);
+  menuButton.value = !menuButton.value;
+};
+
+const timerClose = () => {
+  setTimeout(() => {
+    menuButton.value = false;
+  }, 500);
+};
 
 onMounted(() => {
   store.getsaveProductList();
@@ -35,13 +45,19 @@ watchEffect(() => {
       }"
     >
       <template v-slot:prepend>
-        <!-- <v-btn :icon="mdiDotsVertical" color="black"></v-btn> -->
-        <!-- <v-icon :icon="mdiDotsVertical" /> -->
-        <v-btn icon="mdi-menu" class="d-flex d-sm-none" />
+        <v-btn
+          icon="mdi-menu"
+          @click="toggle"
+          :class="hideCart ? 'd-flex d-sm-none' : 'd-none'"
+        />
       </template>
       <NuxtLink to="/">
         <div class="logo">
-          <img class="logo_img" src="/images/logo.svg" alt="logo" />
+          <img
+            class="logo_img overflow-hidden"
+            src="/images/logo.svg"
+            alt="logo"
+          />
         </div>
       </NuxtLink>
       <v-spacer></v-spacer>
@@ -59,8 +75,22 @@ watchEffect(() => {
       class="d-none d-sm-flex"
     >
       <CategoryList />
-      <!-- <CategoryList /> -->
     </v-navigation-drawer>
+
+    <v-overlay
+      v-model="menuButton"
+      class="align-center justify-center"
+      @click="timerClose"
+      scroll-strategy="close"
+      location-strategy="static"
+    >
+      <div>
+        <v-card>
+          <CategoryList />
+        </v-card>
+      </div>
+    </v-overlay>
+
     <slot />
   </v-layout>
 </template>
@@ -76,6 +106,9 @@ watchEffect(() => {
   }
   @media (min-width: 480px) {
     margin-left: 30px;
+  }
+  @media (max-width: 480px) {
+    width: 50vw;
   }
 }
 .cart {
